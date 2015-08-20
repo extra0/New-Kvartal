e2ddf2f5ed7685297872179999f5438644214c26
Ростислав <extra0@mail.ru>
2015-08-21 00:05:37 +0300

    сделын обьекты на главной

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 $(function(){
 
-	//
+	// плавное перемещение вниз к таблице
+	$('.button__down').click(function(){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+		   scrollTop: $(anchor.attr('href')).offset().top
+		}, 1000);
+	});
+
+	// появление обьектов на карте
+	$('.object__link').click(function(){
+		if ($(this).children('img').hasClass('active')) {
+			return false;
+		} else {
+			$('.object__inner').hide();
+			$('.object__circle').attr('src', 'img/circle.png');
+			$('.object__circle').removeClass('active');
+			$(this).parent().find('.object__inner').fadeIn(500);
+			$(this).children('img').attr('src', 'img/active_circle.png');
+			$(this).children('img').addClass('active');
+			return	false;
+		}
+		
+	});
+
+	// ресайз окна
+	// $(window).resize(function(){
+	// 	return $('.video__play').css('max-height', $(window).height());
+	// 	return $('.video__play').width($(window).width());
+	// });
 
 });
