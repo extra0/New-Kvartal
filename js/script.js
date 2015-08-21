a36d02bdc571842122047e66191490581b72ca64
Ростислав <extra0@mail.ru>
2015-08-21 23:58:12 +0300

    готова страница о компании

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-$(function(){
+$(function() {
 
 	// плавное перемещение вниз к таблице
-	$('.button__down').click(function(){
+	$('.button__down').click(function() {
 		var anchor = $(this);
 		$('html, body').stop().animate({
-		   scrollTop: $(anchor.attr('href')).offset().top
+			scrollTop: $(anchor.attr('href')).offset().top
 		}, 1000);
 	});
 
 	// появление обьектов на карте
-	$('.object__link').click(function(){
+	$('.object__link').click(function() {
 		if ($(this).children('img').hasClass('active')) {
 			return false;
 		} else {
@@ -19,15 +19,36 @@ $(function(){
 			$(this).parent().find('.object__inner').fadeIn(500);
 			$(this).children('img').attr('src', 'img/active_circle.png');
 			$(this).children('img').addClass('active');
-			return	false;
+			return false;
 		}
-		
+	});
+
+	// hover на блок с историей
+	$('.history__block').hover(function(){
+		$(this).toggleClass('hover');
+	});
+
+	$(window).resize(function() {
+		return [ 
+			$('.video__play').width($(window).width()), 
+			$('.video__play').height($(window).height()),
+			$('.video__block').width($(window).width()),
+			$('.video__block').height($(window).height()),
+			$('.full__screen').width($(window).width()),
+			$('.full__screen').height($(window).height())
+		];
 	});
 
 	// ресайз окна
-	// $(window).resize(function(){
-	// 	return $('.video__play').css('max-height', $(window).height());
-	// 	return $('.video__play').width($(window).width());
-	// });
+	return [ 
+		$('.video__play').width($(window).width()), 
+		$('.video__play').height($(window).height()),
+		$('.video__block').width($(window).width()),
+		$('.video__block').height($(window).height()),
+		$('.full__screen').width($(window).width()),
+		$('.full__screen').height($(window).height())
+	];
+
+	
 
 });
