3399db5e4c23da0dd3956fa27cdfd0da2222c8d2
Ростислав <extra0@mail.ru>
2015-08-22 20:26:23 +0300

    готова страница фотогалереи

--- js/script.js
+++ js/script.js
@@ -28,6 +28,28 @@ $(function() {
 		$(this).toggleClass('hover');
 	});
 
+	// галерея 
+	var GammaSettings = {
+		// order is important!
+		viewport: [{
+			width: 1200,
+			columns: 5
+		}, {
+			width: 900,
+			columns: 4
+		}, {
+			width: 500,
+			columns: 3
+		}, {
+			width: 320,
+			columns: 2
+		}, {
+			width: 0,
+			columns: 2
+		}]
+	};
+	Gamma.init(GammaSettings);
+
 	$(window).resize(function() {
 		return [
 			$('.video__play').width($(window).width()),
