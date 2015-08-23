0216194cc265820f9902a4aa971a7e572123b897
Ростислав <extra0@mail.ru>
2015-08-23 21:43:40 +0300

    главная адаптив от 768

--- js/script.js
+++ js/script.js
@@ -34,4 +34,44 @@ $(function() {
 		pagerCustom: '.bx-pager__block',
 		slideWidth: 645
 	});
+
+	// показываем пикторгамку меню
+	var btn = $('.header__menu-btn');
+	if ($(window).width() < 992) {
+		btn.show();
+	} else {
+		btn.hide();
+	}
+	$(window).resize(function(){
+		if ($(this).width() < 992) {
+			btn.show();
+		} else {
+			btn.hide();
+		}
+	});
+
+	// показываем/скрываем меню
+	btn.click(function(){
+		$('.header__menu').slideToggle(500);
+		return false;
+	});
+
+	// закрываем меню вне его области
+	$(document).click(function(event) {
+		if ($(event.target).closest(".header__menu").length) return;
+		$('.header__menu').slideUp(500);
+		indexClick = 0;
+		event.stopPropagation();
+	});
+
+	// закрываем обьекты
+	$(document).click(function(event) {
+		if ($(event.target).closest(".object__inner").length) return;
+		$('.object__inner').fadeOut(500);
+		$('.object__circle').attr('src', 'img/circle.png');
+		$('.object__circle').removeClass('active');
+		indexClick = 0;
+		event.stopPropagation();
+	});
+
 });
